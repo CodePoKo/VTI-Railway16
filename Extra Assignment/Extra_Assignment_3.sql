d1dbc8cb1a1228d5afa1aa1929628911ad8c4647
Thanh Trung <thanhtrungpl95@gmail.com>
2021-09-05 09:04:57 +0700

    upload bai tap

---
+++ Extra Assignment/Extra_Assignment_3.sql
@@ -0,0 +1,2 @@
+-- lấy ra tất cả các thực tập sinh đã vượt qua bài test đầu vào, nhóm chúng thành các tháng sinh khác nhau
+SELECT Full_Name, COUNT()
---
+++ Extra Assignment/InserrtDATA_Extra_Assig_1-2.sql
@@ -0,0 +1,17 @@
+USE fresher_management;
+
+INSERT INTO Trainee
+(Full_Name,			Birth_Date,	 	Gender, 	ET_IQ, 		ET_Gmath, 	ET_English, 	Training_Class,		Evaluation_Notes,  			VTI_Account)
+VALUES	
+(N'Nguyễn Văn A', 	'1998-10-07',	'male', 	14, 		12, 			28,			'001',			N'Đánh giá 1 Đánh giá 1 ',	'vti.a.nguyenvan98'),
+(N'Trần Thị B', 	'2002-12-01',	'female', 	6, 			1, 				43,			'002',			N'Đánh giá 2 Đánh giá 2 ',	'vti.b.thitran02'),
+(N'Hoàng Văn F', 	'1995-02-25',	'female', 	3, 			8, 				49,			'003',			N'Đánh giá 3 Đánh giá 3 ',	'vti.f.vanhoang95'),
+(N'Đỗ Văn Z', 		'2001-10-28',	'unknown', 	9, 			3, 				28,			'004',			N'Đánh giá 4 Đánh giá 4 ',	'vti.z.vando01'),
+(N'Hồng Thị 8', 	'1993-01-31',	'male', 	12, 		15, 			31,			'005',			N'Đánh giá 5 Đánh giá 5 ',	'vti.8.thihong93'),
+(N'Ba Năm Bảy', 	'1994-12-12',	'unknown', 	5, 			20, 			36,			'006',			N'Đánh giá 6 Đánh giá 6 ',	'vti.bay.namba94'),
+(N'Nguyễn Ka L', 	'1987-04-15',	'male', 	8, 			19, 			35,			'007',			N'Đánh giá 7 Đánh giá 7 ',	'vti.l.kanguyen87'),
+(N'Lê Thị ...', 	'2000-04-30',	'female', 	18, 		7, 				44,			'008',			N'Đánh giá 8 Đánh giá 8 ',	'vti.lethi00'),
+(N'T Hi Kh', 		'1999-08-17',	'male', 	20, 		4, 				27,			'009',			N'Đánh giá 9 Đánh giá 9 ',	'vti.kh.thi99'),
+(N'Mười ... Một', 	'1990-02-27',	'unknown', 	11, 		18, 			13,			'010',			N'Đánh giá 10 Đánh giá 10',	'vti.motmuoi.90');
+
+
